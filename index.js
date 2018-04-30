dc010b15188f3f4a5776aeb6c7a9a74ea3127274
Sean Huang <hyprul@ucla.edu>
2018-04-30 03:29:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length  >0){
+   return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+  else return "There is nobody waiting to be served!"
+  
+}
+
+function currentLine(){
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }
+  
+    var s = ""
+    
+    for(let i= 0; i < katzDeliLine.length; i++){
+      s+=`${[i+1]}. ${katzDeliLine[i]}`;
+    }
+    return s
+
+}
