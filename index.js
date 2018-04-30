b3f4e49a3d506d2920236b6c0e84c6b0c14c97ed
Sean Huang <hyprul@ucla.edu>
2018-04-30 06:12:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine){
 function currentLine(){
   if(katzDeliLine.length === 0){
     return "The line is currently empty."
-  }else{
+  }
   
     var s = ""
     
@@ -24,5 +24,5 @@ function currentLine(){
       s+=`${[i+1]}. ${katzDeliLine[i]}`;
     }
     return s
-}
+
 }
